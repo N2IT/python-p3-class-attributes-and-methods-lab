0b166a7848d7bac4d18ad12f19257f6389a051ed
Tony Eder <tonyeder11@gmail.com>
2024-02-15 15:36:49 -0800

    working on penultimate class method - add to genre count

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,53 @@
 class Song:
-    pass
+
+    count = 0
+    genres = []
+    artists = []
+    genre_count = {}
+
+    def __init__(self, name, artist, genre):
+        self.add_song_to_count()
+        self.name = name
+        self.artist = artist
+        self.genre = genre
+        Song.add_to_genre(self)
+        Song.add_to_artists(self)
+        Song.add_to_genre_count(self)
+
+
+    @classmethod
+    def add_song_to_count(cls, increment = 1):
+        cls.count += increment
+
+    @classmethod
+    def add_to_genre(cls, self):
+        cls.genres.append(self.genre)
+        
+    @classmethod
+    def add_to_artists(cls, self):
+        cls.artists.append(self.artist)
+
+    @classmethod
+    def add_to_genre_count(cls, self, increment = 1):
+        breakpoint()
+        g_count = 0
+        cls.genre_count = {
+                    self.genre : 1
+                } 
+        # g_count = 0
+        # for genres in cls.genres:
+        #     if genres == self.genre:
+        #         breakpoint()
+        #         cls.genre_count[value] = cls.genre_count[value] + increment
+        #     else:
+        #         genre_count = {
+        #             self.genre : sum(g_count + increment)
+                # } 
+
+
+        # adds to genre_count where keys are names of each genre
+        # each key points to a value that is the number of songs assigned to that genre
+        
+    @classmethod
+    def add_to_artist_count(cls, self):
+        pass
